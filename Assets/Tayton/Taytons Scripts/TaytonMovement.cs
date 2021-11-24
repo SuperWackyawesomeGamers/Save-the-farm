071578e505cf65e084196812d135e1883604b85a
MUKILTEO\1710005 <Taytonwhite@hotmail.com>
2021-11-24 09:32:06 -0800

    added double jump

--- Assets/Tayton/Taytons Scripts/TaytonMovement.cs
+++ Assets/Tayton/Taytons Scripts/TaytonMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class TaytonMovement : MonoBehaviour
 {
+    public bool DoubleCheck;
     public float RegSpeed = 1;
     public float dashspeed = 12;
     public bool Incooldown;
     public bool dashOK;
     public float MovementSpeed;
     public float JumpForce = 1;
+    public bool Grounded;
+    public int jumpCount;
 
     private Rigidbody2D _rigidbody;
 
@@ -23,12 +26,19 @@ public class TaytonMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        Grounded = Mathf.Abs(_rigidbody.velocity.y) < 0.0001f;
+        if (Grounded)
+        {
+            jumpCount = 0;
+        }
+        
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump") && jumpCount <= 1)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            jumpCount++;
         }
         if (Input.GetKeyDown(KeyCode.LeftShift)&& Incooldown == false)
         {
@@ -38,6 +48,7 @@ public class TaytonMovement : MonoBehaviour
             Incooldown = true;
             Invoke("ResetCooldown", 2.0f);
         }
+        
     }
     private void ResetDash()
     {
