ba2c745b392e250f9f8bcebb879878c8fcdbd3e9
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2021-12-03 08:05:56 -0800

    Fixing my end of the problem with my script

--- Assets/Casey/CTAScripts/CTAanimatorController.cs
+++ Assets/Casey/CTAScripts/CTAanimatorController.cs
@@ -13,19 +13,11 @@ public class CTAanimatorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        myTrans = this.transform;
-        myAnim = this.gameObject.GetComponent<Animator>();
-        instance = this;
-        artScaleCache = myTrans.LocalScale;
+        
     }
     void FlipArt(float currentSpeed)
     {
-        if((currentSpeed < 0 && artScaleCache.x == 1)||
-            (currentSpeed > 0 && artScaleCache.x == -1))
-        {
-            artScaleCache.x *= -1;
-            myTans.localScale = artScaleCache;
-        }
+      
     }
 
     // Update is called once per frame
