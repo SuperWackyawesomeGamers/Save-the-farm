5fbe16cbeca1b518464ea49b9ac76f404adfdcc3
MUKILTEO\1710223 <1710223>
2021-12-06 10:14:13 -0800

    Worked on scene change

---
+++ Assets/Scenes/BackgroundChanger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChanger : MonoBehaviour
+{
+    GameObject[] Director;
+    bool randomBool;
+    private Rigidbody2D rb2;
+    public int pattern;
+
+    void Start()
+    {
+        Director = GameObject.FindGameObjectsWithTag("JLBackgroundactivator");
+        StartCoroutine("JLBackgroundactivator");
+        pattern = 0;
+        InvokeRepeating("ChangePattern", 0, 2f);
+    }
+
+    private void Update()
+    {
+        if (pattern == 0)
+        {
+            Director[0].SetActive(true);
+            Director[1].SetActive(false);
+        }
+        if (pattern == 1)
+        {
+            Director[0].SetActive(false);
+            Director[1].SetActive(true);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            {
+                pattern++;
+            }
+    }
+}
