9c66c1e11be36550a8a7fc2f65cfea9d548a94e8
MUKILTEO\1710005 <Taytonwhite@hotmail.com>
2021-12-08 10:13:55 -0800

    did some credits stuff and broke idle animation lol

---
+++ Assets/Tayton/Taytons Scripts/CreditsUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsUp : MonoBehaviour
+{
+    float yaxis;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yaxis = -10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        yaxis+=Speed;
+        gameObject.transform.position = new Vector3(0, yaxis, 0);
+    }
+}
