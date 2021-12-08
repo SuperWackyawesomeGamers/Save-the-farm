4f78bbbd8b06d1174d8eefd24c122eb8dbae6bfa
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2021-12-08 10:13:21 -0800

    Added Health to everything and enemies spawn in and off when the camera leaves or enters their view.

--- Assets/Casey/CTAScripts/CTAEnemyAI.cs
+++ Assets/Casey/CTAScripts/CTAEnemyAI.cs
@@ -6,12 +6,13 @@ public class CTAEnemyAI : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
+    public CTAEnemyHealth EnemyHealthBar;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
---
+++ Assets/Casey/CTAScripts/CTAEnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CTAEnemyHealth : MonoBehaviour
+{
+    public Slider Enemyslider;
+
+    public void SetMaxHealth(int Health)
+    {
+        Enemyslider.maxValue = Health;
+        Enemyslider.value = Health;
+    }
+
+   public void SetHealth(int health)
+    {
+        Enemyslider.value = health;
+    }
+}
