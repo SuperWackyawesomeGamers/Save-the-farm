4928c758a439cc604c537ee79c394c58e4961162
MUKILTEO\1710438 <1710438@mukilteo.wednet.edu>
2021-12-10 10:09:22 -0800

    BossHelthWIP

---
+++ Assets/Taylor/bosshealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bosshealth : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Slider healthBar;
+    public Rigidbody2D rb2;
+    public Movement m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        currentHealth = 100;
+        healthBar.GetComponent<Healthbarslider>().SetMaxHealth(maxHealth);
+        rb2 = GetComponent<Rigidbody2D>();
+        m = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(1);
+        }
+
+
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+
+        healthBar.GetComponent<Healthbarslider>().SetHealth(currentHealth);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+
+            TakeDamage(12);
+        }
+
+    }
+}
