f0b2bc80ea340fc0bf39a7248ff5f2c4057a2c0b
MUKILTEO\1710223 <1710223>
2021-12-14 09:14:58 -0800

    Hope to not merge error

---
+++ Assets/Scenes/Scripts/JLEnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JLEnemyAI : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public CTAEnemyHealth EnemyHealthBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        EnemyHealthBar.SetMaxHealth(maxHealth);
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bullet")
+        {
+            TakeDamage(1);
+        }
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        EnemyHealthBar.SetHealth(currentHealth);
+    }
+}
