5939a2cd026d8fedaf15a31b421028d90049c7ca
MUKILTEO\1710005 <Taytonwhite@hotmail.com>
2021-12-14 09:41:52 -0800

    did a bunch of stuff lol

---
+++ Assets/Tayton/Taytons Scripts/SceneChanger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main menu");
+    }
+}
+// Start is called before the first frame update
