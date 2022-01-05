6ca876f77d0e265bce55d48c2a454b5204996dfa
MUKILTEO\1710223 <1710223>
2022-01-05 09:38:06 -0800

    working on final boss

---
+++ Assets/Scenes/Scripts/bossshooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossshooting : MonoBehaviour
+{
+    private Rigidbody2D rb2;
+    public GameObject Bullet;
+    public GameObject FirePoint;
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scenes/Scripts/finalbossmovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finalbossmovement : MonoBehaviour
+{
+    private bool dirRight = true;
+    public float speed = 2.0f;
+    public float JumpForce;
+    private Rigidbody2D rigidbody;
+    public GameObject Bullet;
+    public GameObject FirePoint;
+    public float bulletSpeed;
+    public GameObject Firedirection;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime * 2);
+        else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime * 2);
+
+        if (transform.position.x >= 7.0f)
+        {
+            dirRight = false;
+            rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            GameObject b = Instantiate(Bullet, FirePoint.transform.position, Quaternion.identity);
+            Rigidbody2D rb2bullet = b.GetComponent<Rigidbody2D>();
+            float spray = Random.Range(-0.5f, 0.5f);
+            rb2bullet.AddForce(bulletSpeed * (transform.right + new Vector3(0, spray, 0)));
+            Destroy(b, 1f);
+        }
+
+        if (transform.position.x <= -7)
+        {
+            dirRight = true;
+            rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            GameObject b = Instantiate(Bullet, FirePoint.transform.position, Quaternion.identity);
+            Rigidbody2D rb2bullet = b.GetComponent<Rigidbody2D>();
+            float spray = Random.Range(-0.5f, 0.5f);
+            rb2bullet.AddForce(bulletSpeed * (transform.right + new Vector3(0, spray, 0)));
+            Destroy(b, 1f);
+        }
+    }
+}
