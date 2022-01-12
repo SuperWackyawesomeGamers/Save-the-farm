a0ed1e01525a1a3c1df98d43035d6ec022c37d1e
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2022-01-12 10:09:33 -0800

    PuffBall Enemy is in! its a tank though and i dont know why.

--- Assets/Casey/CTAScripts/FastBean.cs
+++ Assets/Casey/CTAScripts/FastBean.cs
@@ -1,27 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class FastBean : MonoBehaviour
 {
-
+    public CTAPlayerMove CTA;
+    public float targetTime = 200.0f;
+    private bool powerOff = false;
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        print(targetTime);
+        if (powerOff == true)
+        {
+            CTA.MovementSpeed = 4;
+        }
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject);
         {
+            CTA.MovementSpeed += 1;
             Destroy(this.gameObject);
+            powerOff = false;
+            targetTime -= Time.deltaTime;
+
+            if (targetTime <= 0.0f)
+            {
+                timerEnded();
+            }
         }
     }
-
+    void timerEnded()
+    {
+        powerOff = true;
+    }
 }
